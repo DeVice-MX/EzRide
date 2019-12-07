c6b97e3d547983c55e07fdc8c11f9dbdd0da2f24
AlexMethod <alvalex_@hotmail.com>
2019-12-06 23:53:19 -0600

    finalizacion viaje

--- js/finalizacionViaje.js
+++ js/finalizacionViaje.js
@@ -1,4 +1,27 @@
 $(function(){
 
+    $(document).on('click','.input-star',fnShowMessage);
+
 });
+
+var fnShowMessage = function(e){
+    let value = $(this).val();
+    if(value == "1"){
+        $('#message').html('<i class="far fa-sad-cry text-center"></i>');
+    }
+    else if(value == "2"){
+        $('#message').html('<i class="far fa-surprise"></i>');
+    }
+    else if(value == "3"){
+        $('#message').html('<i class="far fa-smile-beam"></i>');
+    }
+    else if(value == "4"){
+        $('#message').html('<i class="far fa-laugh-wink"></i>');
+    }
+    else if(value == "5"){
+        $('#message').html('<i class="far fa-kiss-wink-heart"></i>');
+    }
+}
+
+
 
