dd88a9c77623dffe3d6db06dd8e08b57fc6aca53
Dennise Monserrath Ibarra Cuevas <53837664+denscuevs@users.noreply.github.com>
2019-12-07 05:05:10 -0600

    Perfiles de Usuario y Conductor

--- js/login.js
+++ js/login.js
@@ -1,9 +1,21 @@
 
 $(function () {
 
-    $('#btn-submitLogin').on('click', function() {
-        $("#login").valid();
-    });
-
+    
 });
 
+$(document).ready(function() {
+    // $('#login').validate({ // initialize the plugin
+    //     rules: {
+    //         name: {
+    //             required: true,
+    //             email: true
+    //         },
+    //         password: {
+    //             required: true,
+    //             minlength: 7
+    //         }
+    //     }
+    // });
+
+})
