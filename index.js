8c9370900c3b0bf56d3e6bd797f19a3fd50878e2
AlexMethod <alvalex_@hotmail.com>
2019-12-06 18:12:42 -0600

    Conexion correcta con Datos!

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const mongoose = require('mongoose');
 const app = require('./app');
 const config = require('./config');
 
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = process.env.DATABASE;
+
+// MongoClient.connect(uri, function(err, db) {
+//   db.close();
+// });
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
 mongoose.connect(config.db, { useNewUrlParser: true })
 .then((res) => {
 	console.log("Mongo connected!");
@@ -13,7 +29,7 @@ mongoose.connect(config.db, { useNewUrlParser: true })
 	});
 
 	app.listen(config.port, () => {
-		console.log(`API EZRIDE JALANDO EN:${config.port}`);
+		console.log(`API EZRIDEq JALANDO EN:${config.port}`);
 	});
 })
 .catch((err) => {
