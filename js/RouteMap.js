c350d6f709fa34f8cf07cba1fe0be7d408db3e19
Daniel Torres <youkiddingd@gmail.com>
2019-12-07 11:42:04 -0600

    up

--- js/RouteMap.js
+++ js/RouteMap.js
@@ -162,7 +162,8 @@ var fnGetViajes = function () {
                                 <div class="card-header">
                                     <span class="mb-0">
                                             Viaje #${index + 1}
-                                            <span>Fecha: ${moment(viaje.t_Alta).format("dddd, MMMM Do YYYY, h:mm:ss a")}</span>
+                                            Tarifa $${viaje.tarifa}
+                                            <span>Fecha: ${moment(viaje.t_Salida + viaje.h_Salida).format("dddd, MMMM Do YYYY, h:mm:ss a")}</span>
                                             <button class="btn btn-primary">Seleccionar
                                             </button>
                                     </span>
