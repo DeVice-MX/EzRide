993a8c3c615f1b5f24af2d8113c7136be766ae90
AlexMethod <alvalex_@hotmail.com>
2019-12-06 21:30:33 -0600

    fetchs

---
+++ controllers/viaje.js
@@ -0,0 +1,27 @@
+'use strict'
+const routeApi = 'http://localhost:3000/';
+/*GETS-----------------------------------------*/
+async function REQUEST(url, method, jParams) {
+    let options = {
+        method: method
+    };
+    if (method != 'GET') {
+        options['headers'] = { 'Content-Type': 'application/json' };
+        options['body'] = JSON.stringify(jParams);
+    }
+    let response = await fetch(url, options);
+    if (response.ok) {
+        let data = await response.json();
+        return data;
+    } else {
+        //var jsonSwal = enumSwal.error[getCookie("idioma")];
+        //swal(jsonSwal).then(function () {
+
+        //});
+    }
+}
+
+function getAllViajes(jParams){
+    let hola = 0;
+    return REQUEST(`${routeApi}Viaje?${$.param(jParams)}`);
+}
